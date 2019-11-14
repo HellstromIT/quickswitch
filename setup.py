d042d2df927e4084eded88c4d1b4e7f10ab8409b
Martin Hellstrom <martin@hellstrom.it>
2019-11-14 11:05:59 +0000

    Add gitignore and setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='quickswitch',
+    version='0.1',
+    scripts=['qs'] ,
+    author="Martin Hellstrom",
+    author_email="martin@hellstrom.it",
+    description="A utility to list directories within directories. Useful with fzf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hellstromit/quickswitch",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
